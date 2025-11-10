6be7b5c9e0252a75a50885f6d2b0bfe53bbc2267
KonstPartner <konst.partnership@gmail.com>
2025-11-10 01:27:49 -0800

    wip(refactoring):  theme primary color

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,3 +1,19 @@
 import { createTheme } from '@mantine/core';
 
-export const theme = createTheme({});
+export const theme = createTheme({
+  primaryColor: 'orange',
+  colors: {
+    orange: [
+      '#FFF4E6',
+      '#FFE8CC',
+      '#FFD8A8',
+      '#FFC078',
+      '#FFA94D',
+      '#FF922B',
+      '#FD7E14',
+      '#F76707',
+      '#E8590C',
+      '#D9480F',
+    ],
+  },
+});
